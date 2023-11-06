e3b0577cc5d8deba7b8216036b43832cfc0b562b
Raen Reyes <raen.reyes11@outlook.com>
2023-11-06 10:18:04 -0800

    Add project files.

---
+++ Functions_Methods/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Functions_Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
