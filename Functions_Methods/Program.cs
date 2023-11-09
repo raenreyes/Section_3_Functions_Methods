25d88cf40b5e2a6c4cf88f038bd52aaf1dd8813c
Raen Reyes <raen.reyes11@outlook.com>
2023-11-09 00:26:18 -0800

    try and catch and finally

--- Functions_Methods/Program.cs
+++ Functions_Methods/Program.cs
@@ -6,25 +6,37 @@ namespace Functions_Methods
     {
         static void Main(string[] args)
         {
-           
 
-            Console.WriteLine(AddNumbers());
+
+            Console.WriteLine("Please enter a number!");
+            string userInput = Console.ReadLine();
+
+            try //you want code to be tried and you dont want your program to crash
+            {
+                int userInputAsInt = int.Parse(userInput);
+            }
+            catch (FormatException) //if the code did not work then the catch will execute
+            {
+                Console.WriteLine("Format exception please enter the correct type next time!");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Overflow exception number was either too small or too large!");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("General Exception!");
+            } finally //finally happens when the try and catch statement is done. This closes the statmetn which is very important
+            {
+                Console.WriteLine("This is called anyways");
+            }
+          
+        
+
+
+
 
         }
-        public static int AddNumbers ()
-        {
-            Console.WriteLine("Please enter your first number you would like to add: ");
-            string input1 = Console.ReadLine();
-
-            Console.WriteLine("Please enter your second number you would like to add: ");
-            string input2 = Console.ReadLine();
-            int number1 = int.Parse(input1);
-            int number2 = int.Parse(input2);
-            
-            int result = number1 + number2;
-            return result;
-
-  
-        }
+       
     }
 }
